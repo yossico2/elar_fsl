d44b74b49efd53b8321ab5deeee68cd226fe17e6
Yossi Cohen <yossico@rafael.co.il>
2025-12-11 16:53:30 +0200

    ref

--- run_fsl.sh
+++ run_fsl.sh
@@ -3,11 +3,22 @@
 set -e
 
 function main() {
-	instance="${1:-1}"
-	docker run --rm -it --network=host \
+	detach=""
+	instance=1
+
+	# parse args in any order
+	for arg in "$@"; do
+		if [[ "$arg" == "-d" ]]; then
+			detach="-d"
+		elif [[ "$arg" =~ ^[0-9]+$ ]]; then
+			instance="$arg"
+		fi
+	done
+
+	docker run --rm ${detach} -it --network=host \
 		--name "fsl-${instance}" \
 		-e SENSOR_INSTANCE="${instance}" \
-		"fsl"
+		fsl
 }
 
 main "$@"
